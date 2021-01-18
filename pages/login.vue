9629e6585280c9fe0c072195a32d5fcecbff0727
Pranav Raut <pranavraut033@gmail.com>
2021-01-18 19:18:26 +0530

    /fix login issue showing blank
    Signed-off-by: Pranav Raut <pranavraut033@gmail.com>

--- pages/login.vue
+++ pages/login.vue
@@ -108,9 +108,9 @@ export default {
     }
   },
   mounted() {
-    if (this.$route.query.brandName) this.$router.replace({ query: null })
-
     this.init()
+
+    if (this.$route.query.brandName) this.$router.replace({ query: null })
   },
   methods: {
     init(ready) {
