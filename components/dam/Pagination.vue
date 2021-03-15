2f60b3639eed2f6a6f8eb7a05f5a3d655f63420c
Pranav Raut <pranavraut033@gmail.com>
2021-03-15 13:19:53 +0530

    DAM-291 update pagination
    Signed-off-by: Pranav Raut <pranavraut033@gmail.com>

--- components/dam/Pagination.vue
+++ components/dam/Pagination.vue
@@ -1,53 +1,93 @@
 <template>
   <div class="pagination-container">
     <ul class="pagination">
-      <!-- <li v-if="page > 2" class="page-item" @click="page = 1">
-        <a class="page-link">
-          <i class="fa fa-angle-double-left" aria-hidden="true"></i>
-        </a>
-      </li> -->
       <li v-if="page > 1" class="page-item" @click="page -= 1">
         <a class="page-link">
           <i class="fa fa-angle-left" aria-hidden="true"></i>
         </a>
       </li>
-      <li
-        v-for="i in lastPage"
-        :key="i"
-        class="page-item"
-        :class="{ active: page == i }"
-        @click="page = i"
-      >
-        <a class="page-link">{{ i }}</a>
-      </li>
+      <template v-for="link in links">
+        <li v-if="link.separator" :key="link.key" class="py-1 mb-2">
+          <span>...</span>
+        </li>
+        <li
+          v-else
+          :key="link.index"
+          :class="{ active: page == link.index }"
+          class="page-item"
+          @click="page = link.index"
+        >
+          <a class="page-link">{{ link.index }}</a>
+        </li>
+      </template>
       <li v-if="page != lastPage" class="page-item" @click="page += 1">
         <a class="page-link">
           <i class="fa fa-angle-right" aria-hidden="true"></i>
         </a>
       </li>
-      <!-- <li
-        v-if="page != lastPage - 1"
-        class="page-item"
-        @click="page = lastPage"
-      >
-        <a class="page-link">
-          <i class="fa fa-angle-double-right" aria-hidden="true"></i>
-        </a>
-      </li> -->
     </ul>
   </div>
 </template>
 
 <script>
+const MAX_PAGE = 7
+
 export default {
   props: {
-    lastPage: { type: Number, default: 1 },
-    currentPage: { type: Number, default: 1 },
+    lastPage: { type: Number, default: -1 },
+    currentPage: { type: Number, default: -1 },
   },
   data() {
     return {
       page: this.currentPage,
     }
+  },
+  computed: {
+    links() {
+      const links = []
+      const mid = Math.round(MAX_PAGE / 2)
+
+      if (this.lastPage <= MAX_PAGE) {
+        Array(this.lastPage)
+          .fill(null)
+          .forEach((_, i) => links.push({ index: i }))
+      } else if (this.page < mid) {
+        Array(mid)
+          .fill(null)
+          .forEach((_, i) => links.push({ index: i + 1 }))
+
+        links.push({ separator: true, key: 'mid-sep' })
+        const startEnd = this.lastPage - mid + 1
+
+        Array(mid - 1)
+          .fill(null)
+          .forEach((_, i) => links.push({ index: startEnd + i + 1 }))
+      } else if (this.page >= mid && this.page <= this.lastPage - mid) {
+        links.push({ index: 1 })
+        if (this.page >= mid) links.push({ separator: true, key: 'start-sep' })
+        Array(MAX_PAGE - 2)
+          .fill(null)
+          .forEach((_, i) =>
+            links.push({ index: this.page + i + 1 - Math.round(mid / 2) })
+          )
+
+        if (this.page + mid < this.lastPage)
+          links.push({ separator: true, key: 'end-sep' })
+        links.push({ index: this.lastPage })
+      } else {
+        Array(mid - 2)
+          .fill(null)
+          .forEach((_, i) => links.push({ index: i + 1 }))
+
+        links.push({ separator: true, key: 'mid-sep' })
+        const startEnd = this.lastPage - mid
+
+        Array(mid + 1)
+          .fill(null)
+          .forEach((_, i) => links.push({ index: startEnd + i }))
+      }
+      return links
+    },
   },
   watch: {
     page(page) {
