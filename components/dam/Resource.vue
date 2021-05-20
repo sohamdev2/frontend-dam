ed1b30baaab6fc3ab4e2df32b5c0a8e5d712876a
unknown <saumya@rivuletdigital.com>
2021-05-20 19:42:43 +0530

    DAM-485, DAM-483

--- components/dam/Resource.vue
+++ components/dam/Resource.vue
@@ -196,7 +196,7 @@
           <div class="video-info">
             <div class="upper-info">
               <span :inner-html.prop="file.file_type || '&dash;'"></span>
-              <a
+              <!-- <a
                 v-if="!shareMode"
                 @click="
                   emitShare
@@ -205,7 +205,50 @@
                 "
               >
                 <img src="~/assets/img/share.svg" alt="" class="white-icon" />
-              </a>
+              </a> -->
+              <div class="d-flex align-items-center">
+                <div
+                  class="dropdown more-options"
+                  :class="{ show: dropDownList }"
+                >
+                  <button
+                    type="button"
+                    class="dropdown-toggle"
+                    data-toggle="dropdown"
+                    @click.capture.stop="dropDown()"
+                  >
+                    <img src="~/assets/img/menu-option.svg" alt="" />
+                  </button>
+                  <ul class="dropdown-menu" :class="{ show: dropDownList }">
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        data-toggle="modal"
+                        data-target="#sharePopup"
+                        @click.capture.stop="selectFromDrop(file, 'share')"
+                        ><span class="dropdown-item-icon"
+                          ><img
+                            src="~/assets/img/share.svg"
+                            alt=""
+                            class="img-responsive" /></span
+                        >Share</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        @click.capture.stop="selectFromDrop(file, 'download')"
+                        ><span class="dropdown-item-icon"
+                          ><img
+                            src="~/assets/img/download.svg"
+                            alt=""
+                            class="img-responsive" /></span
+                        >Download</a
+                      >
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
             <div class="down-info">
               <a
@@ -339,6 +382,7 @@ export default {
       videoThumbnailAdded: false,
       imageLoading: false,
       videoThumbnailFetching: false,
+      dropDownList: false,
     }
   },
   computed: {
@@ -393,6 +437,19 @@ export default {
     })
   },
   methods: {
+    // dropdown feature
+    selectFromDrop(file, type) {
+      if (type === 'share') {
+        this.$refs.shareDialog.toggleModel()
+      } else if (type === 'download') {
+        // this.$emit('selectedDrop', file, type, 'file')
+        this.downloadFile()
+      }
+    },
+    // display of dropdown menu
+    dropDown() {
+      this.dropDownList = !this.dropDownList
+    },
     isPlaying() {
       if (!this.isVideo) return false
 
