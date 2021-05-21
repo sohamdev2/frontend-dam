c01b3eeb1d96f2be662e61f9ff1657319c7cba2b
hitaliribadiya <82148550+hitaliribadiya@users.noreply.github.com>
2021-05-21 17:56:48 +0530

    Few Files are not downloaded so debug in the dam frontend.

--- pages/get_assets.vue
+++ pages/get_assets.vue
@@ -11,13 +11,11 @@ export default {
   }) {
     const downloadURL = `object-download?${$toQueryString(query)}`
     $marcomAxios.setHeader('Authorization', $auth.strategy.token.get())
-
     await $marcomAxios
       .$get(downloadURL)
       .then((data) => redirect(data))
       .catch(({ response, message }) => {
         const { status, data } = response || {}
-
         error({ status: status || 500, message: data?.message || message })
       })
   },
