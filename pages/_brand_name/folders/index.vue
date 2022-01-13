74b3a85bf25509b4d4bc91040301301bee668074
sohamdev2 <soham@rivuletdigital.com>
2022-01-13 16:26:54 +0530

    Resource selection on action fixed

--- pages/_brand_name/folders/index.vue
+++ pages/_brand_name/folders/index.vue
@@ -4,69 +4,6 @@
       <h4>Folders</h4>
 
       <div class="category-list customscrollbar">
-        <!-- <h4
-          v-if="
-            currentFolder &&
-            $refs.folderList &&
-            $refs.folderList.getCurrentFolderName()
-          "
-          class="title"
-        >
-          <nuxt-link
-            :to="{
-              name: 'brand_name-folders',
-              params: { brand_name: $getBrandName() },
-              hash: `#${currentFolder.parent_id || ''}`,
-            }"
-            class="back"
-          >
-            <svg
-              id="Layer_1"
-              class="back-icon blue"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              x="0px"
-              y="0px"
-              viewBox="0 0 18 18"
-              xml:space="preserve"
-            >
-              <g id="Group_4468" transform="translate(-577 -181)">
-                <rect
-                  id="Rectangle_3007"
-                  x="577"
-                  y="181"
-                  class="fill-hide"
-                  width="18"
-                  height="18"
-                ></rect>
-                <g id="Group_4425" transform="translate(-514.168 -476.583)">
-                  <path
-                    id="Icon_material-backspace"
-                    class="fill-color"
-                    d="M1107.7,659.8h-11.3c-0.5,0-0.9,0.3-1.2,0.7l-4.1,6.1l4.1,6.1c0.3,0.4,0.7,0.7,1.2,0.7h11.3c0.8,0,1.5-0.7,1.5-1.5v-10.5C1109.2,660.5,1108.5,659.8,1107.7,659.8z"
-                  ></path>
-                  <g id="Group_4424">
-                    <g id="Group_4422">
-                      <path
-                        id="Path_3527"
-                        class="fill-white"
-                        d="M1099.7,669.8c-0.4,0-0.7-0.3-0.7-0.8c0-0.2,0.1-0.4,0.2-0.5l5-5c0.3-0.3,0.8-0.3,1.1,0c0.3,0.3,0.3,0.7,0,1l-5,5C1100.1,669.8,1099.9,669.8,1099.7,669.8z"
-                      ></path>
-                    </g>
-                    <g id="Group_4423">
-                      <path
-                        id="Path_3528"
-                        class="fill-white"
-                        d="M1104.7,669.8c-0.2,0-0.4-0.1-0.5-0.2l-5-5c-0.3-0.3-0.3-0.8,0-1.1c0.3-0.3,0.8-0.3,1.1,0c0,0,0,0,0,0l5,5c0.3,0.3,0.3,0.8,0,1.1C1105.1,669.8,1104.9,669.8,1104.7,669.8L1104.7,669.8z"
-                      ></path>
-                    </g>
-                  </g>
-                </g>
-              </g></svg
-          ></nuxt-link>
-          {{ $refs.folderList.getCurrentFolderName() }}
-        </h4> -->
         <FolderList></FolderList>
       </div>
       <ul class="quick-links">
@@ -161,14 +98,10 @@
             <rect x="117.4" y="15" rx="1" ry="1" width="112.4" height="112.4" />
             <rect x="234.9" y="15" rx="1" ry="1" width="112.4" height="112.4" />
             <rect x="352.3" y="15" rx="1" ry="1" width="112.4" height="112.4" />
-            <!-- <rect x="0" y="60" rx="1" ry="1" width="40" height="40" />
-                  <rect x="0" y="105" rx="1" ry="1" width="40" height="40" />
-                  <rect x="0" y="105" rx="1" ry="1" width="40" height="40" /> -->
           </template>
         </ContentLoader>
       </div>
       <template>
-        <!-- <template v-show="!loading"> -->
         <div v-if="noData" key="no-data" class="no-data-found">
           <div class="inner w-100">
             <svg
@@ -490,8 +423,8 @@ export default {
     },
     // dropdown functionality
     dropDown(file, type, resourceType) {
-      this.selectedFiles = []
-      this.selectedFolders = []
+      // this.selectedFiles = []
+      // this.selectedFolders = []
     },
     nextLocalPage($state) {
       if (this.localPage > this.localTotalPages) {
