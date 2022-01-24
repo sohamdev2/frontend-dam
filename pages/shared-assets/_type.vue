b30b6d74d1f9c58192549694ab4feeaad2b6e229
Hitali Ribadiya <hitali@rivuletdigital.com>
2022-01-24 15:46:36 +0530

    Shared URL breadcrumbs add in dam frontend.

--- pages/shared-assets/_type.vue
+++ pages/shared-assets/_type.vue
@@ -5,26 +5,73 @@
         <h2 class="title pl0">Shared File and folders</h2>
       </div>
       <div
-        v-if="allAssetsCount"
-        class="btns mb20 d-flex align-items-center justify-content-end"
+        class="btns mb20 d-flex align-items-center"
+        :class="{
+          'justify-content-between': breadcrumb.length,
+          'justify-content-end': !breadcrumb.length,
+        }"
       >
-        <a
-          v-if="
-            allAssetsCount &&
-            (selectedCount == 0 || selectedCount == allAssetsCount)
-          "
-          href="javascript:void(0);"
-          class="btn"
-          @click="downloadSelectedMultipleFiles()"
-          >Download All</a
-        >
-        <a
-          v-if="selectedCount && selectedCount != allAssetsCount"
-          href="javascript:void(0);"
-          class="btn"
-          @click="downloadSelectedMultipleFiles()"
-          >Download ({{ selectedCount }})</a
-        >
+        <div v-if="breadcrumb.length">
+          <a href="share-inner.html" class="home-icon">
+            <svg
+              id="Layer_1"
+              class="home-icon orange"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 16.2 18"
+              xml:space="preserve"
+            >
+              <g id="Group_4409" transform="translate(-871.194 -659.595)">
+                <g id="Path_3470">
+                  <path
+                    id="Path_3514"
+                    class="fill-color"
+                    d="M879.3,661.5l6.6,5.1v9.2c0,0.2-0.1,0.3-0.3,0.3H873c-0.2,0-0.3-0.1-0.3-0.3v-9.2L879.3,661.5M879.3,659.6l-8.1,6.3v9.9c0,1,0.8,1.8,1.8,1.8h12.6c1,0,1.8-0.8,1.8-1.8v-9.9L879.3,659.6z"
+                  ></path>
+                </g>
+                <g id="Path_3471">
+                  <path
+                    id="Path_3515"
+                    class="fill-color"
+                    d="M882,677.3c-0.4,0-0.8-0.3-0.8-0.8v-7.2h-3.9v7.2c0,0.4-0.3,0.8-0.8,0.8s-0.8-0.3-0.8-0.8v-8c0-0.4,0.3-0.8,0.8-0.8h5.4c0.4,0,0.8,0.3,0.8,0.8v8C882.7,677,882.4,677.3,882,677.3z"
+                  ></path>
+                </g>
+              </g>
+            </svg>
+          </a>
+          <div class="breadcrumb-links">
+            <ul>
+              <li>&nbsp;</li>
+              <li><a href="#">New Folder</a></li>
+              <li><a href="#">New Folder 2</a></li>
+              <li><a href="#">New Folder 3</a></li>
+              <li><a href="#">New Folder 4</a></li>
+              <li><span>Ambulance Lead Times</span></li>
+            </ul>
+          </div>
+        </div>
+        <div v-if="allAssetsCount">
+          <a
+            v-if="
+              allAssetsCount &&
+              (selectedCount == 0 || selectedCount == allAssetsCount)
+            "
+            href="javascript:void(0);"
+            class="btn"
+            @click="downloadSelectedMultipleFiles()"
+            >Download All</a
+          >
+          <a
+            v-if="selectedCount && selectedCount != allAssetsCount"
+            href="javascript:void(0);"
+            class="btn"
+            @click="downloadSelectedMultipleFiles()"
+            >Download ({{ selectedCount }})</a
+          >
+        </div>
       </div>
 
       <div
@@ -178,6 +225,7 @@ export default {
       mode: 'column',
       deleting: false,
       loading: false,
+      breadcrumb: [],
     }
   },
   computed: {
