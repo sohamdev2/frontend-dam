b6edb37dc47f3d8a76188b5ac6e4c50172b77a44
Hitali Ribadiya <hitali@rivuletdigital.com>
2022-04-27 15:06:26 +0530

    bugs

--- components/dam/SharedFolder.vue
+++ components/dam/SharedFolder.vue
@@ -47,7 +47,7 @@
     </div>
     <div class="assets tb-column flex15">
       <div class="top-column">
-        <span
+        <span style="text-transform: unset"
           ><template v-if="assetsCount">
             <template v-if="folder.sub_category_count > 0"
               >{{ folder.sub_category_count }} Folder<template
